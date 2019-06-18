39f63a5d6b1d3f4567bec38d8e64de7e0bd3be7c
ShogunRoss <nguyenconglong95@gmail.com>
2019-06-18 11:42:38 +0700

    + Add frontend build

--- src/app.js
+++ src/app.js
@@ -1,48 +1,59 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const graphqlHttp = require('express-graphql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
+const graphqlHttp = require("express-graphql");
 
-const graphQlSchema = require('./graphql/schema');
-const grapQlResolvers = require('./graphql/resolvers');
+const graphQlSchema = require("./graphql/schema");
+const grapQlResolvers = require("./graphql/resolvers");
 
-const isAuth = require('./middleware/is-auth');
+const isAuth = require("./middleware/is-auth");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	if (req.method === 'OPTIONS') {
-		return res.sendStatus(200);
-	}
-	next();
-})
+app.use(express.static(path.join(__dirname, "frontend")));
 
-app.route('/')
-	.get(function (req, res) {
-		res.status(200).send("Hydro Hub Server is ready.");
-	});
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+
+// app.route('/')
+// 	.get(function (req, res) {
+// 		res.status(200).send("Hydro Hub Server is ready.");
+// 	});
 
 app.use(bodyParser.json());
 
 app.use(isAuth);
 
-app.use('/hydro-hub', graphqlHttp({
-  schema: graphQlSchema,
-  rootValue: grapQlResolvers,
-  graphiql: true
-}));
+app.use(
+  "/hydro-hub",
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: grapQlResolvers,
+    graphiql: true
+  })
+);
 
-mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true })
+mongoose
+  .connect(process.env.MONGO_DB, { useNewUrlParser: true })
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Connected to Database. Server is running on port ${PORT}`)
+      console.log(`Connected to Database. Server is running on port ${PORT}`);
     });
   })
   .catch(err => {
     console.log(err);
-  })
+  });
